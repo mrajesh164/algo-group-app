201279e758c1d585b956ceaf5ae3846662f16ec1
mrajesh164 <mrajesh@uchicago.edu>
2024-10-09 17:35:12 -0500

    adding test file

---
+++ find-duplicate/test_option1.py
@@ -0,0 +1,18 @@
+import pytest
+from option1.py import findDuplicate_one
+from option1.py import findDuplicate_two
+
+t1 = []
+t2 = [1]
+t3 = [1, 7, 3, 4, 2, 4, 6, 5]
+t4 = [3, 3, 2, 1]
+
+def test_cases():
+    assert findDuplicate_one(t1) == -1
+    assert findDuplicate_two(t1) == -1
+    assert findDuplicate_one(t2) == -1
+    assert findDuplicate_two(t2) == -1
+    assert findDuplicate_one(t3) == 4
+    assert findDuplicate_two(t3) == 4
+    assert findDuplicate_one(t4) == 3
+    assert findDuplicate_two(t4) == 3
