55e9ec7b810f1e8cfeab0c5a4d28f31e179cb36b
mrajesh164 <mrajesh@uchicago.edu>
2024-10-09 22:24:51 -0500

    final edits

--- find-duplicate/test_option1.py
+++ find-duplicate/test_option1.py
@@ -16,3 +16,17 @@ def test_cases():
     assert findDuplicate_two(t3) == 4
     assert findDuplicate_one(t4) == 3
     assert findDuplicate_two(t4) == 3
+
+"""
+test results:
+Mayas-MacBook-Pro:find-duplicate maya$ pytest
+====================================================================== test session starts =======================================================================
+platform darwin -- Python 3.12.6, pytest-8.1.1, pluggy-1.4.0
+rootdir: /Users/maya/Documents/algo-group-take-home/find-duplicate
+plugins: json-report-1.5.0, metadata-3.1.1, mypy-0.10.3, timeout-2.3.1
+collected 1 item                                                                                                                                                 
+
+test_option1.py .                                                                                                                                          [100%]
+
+======================================================================= 1 passed in 0.01s ========================================================================
+"""
